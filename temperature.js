599c51791af7fb258a8ea855e3491266932fdc7e
Ben Barber <barberboy@gmail.com>
2016-09-12 02:03:29 +0000

    Add temperature.js which converts between celsius and fahrenheit.

---
+++ temperature.js
@@ -0,0 +1,15 @@
+var Temperature = {
+    toFahrenheit: function ( celsius ) {
+        return ( celsius * (9 / 5) ) + 32;
+    },
+    toCelsius: function (fahrenheit) {
+        return (fahrenheit - 32) * (5 / 9)
+    }
+};
+
+var tempToday = 32;
+var tempInCelsius = Math.round( Temperature.toCelsius( tempToday ) );
+
+var message = `Today’s temperature is ${tempToday}°F, which is ${tempInCelsius}°C.`;
+
+console.log(message);
